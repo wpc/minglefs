e37293a50ad3f3be21cc1bcdd52f9f5c460ca9e6
wpc <alex.hal9000@gmail.com>
2008-05-15 02:37:16 +0800

    oops, forget files

--- lib/mingle_fs/commands/load.rb
+++ lib/mingle_fs/commands/load.rb
@@ -1,14 +1,18 @@
 module MingleFs
   module Commands
     class Load < CommandLine::Command
+      LOADERS = {
+        'eval:' => lambda {|args| eval(args.join(" ")) },
+        'rest:' => lambda {|args| Mingle::Card.find(:all) }
+      }
+      
       def self.aliases
         ['l', 'load']
       end
       
       def execute(*args)
-        type = args.shift
-        data_script = args.join " "
-        context.repository.push(*eval(data_script))
+        loader = LOADERS[args.shift] || raise('unknown type of loader')
+        context.repository.push(*(loader.call(args)))
       end
     end
   end
